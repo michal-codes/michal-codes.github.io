753a60522b2d4a1451b5abc114cb18b29eaffda2
Michal Codes <michal.codes@gmail.com>
2019-06-24 18:47:57 +0200

    initial commit to personal site repo

---
+++ precache-manifest.48c0eb4c987f347753b9c420c3a0040c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5386ac0bae469f574495c6bf4e1c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93718a05670d78801580",
+    "url": "/static/css/main.30cc211a.chunk.css"
+  },
+  {
+    "revision": "897d52570ff53320a63a",
+    "url": "/static/js/2.85ef78b2.chunk.js"
+  },
+  {
+    "revision": "93718a05670d78801580",
+    "url": "/static/js/main.351a13c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
